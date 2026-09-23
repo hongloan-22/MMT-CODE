0d469a15255d4f65fd355913104cdd74625785f9
daumoidayy666 <daumoiday2089@gmail.com>
2026-09-24 02:30:40 +0700

    US14:Tao cac Entity BusRoute,BusStop,RouteStop

---
+++ Ticket/Ticket/Models/BusRoute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Smart_Bus_Ticketing_System.Models
+{
+    public class BusRoute
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên tuyến không được để trống")]
+        [StringLength(150)]
+        public string RouteName { get; set; } = string.Empty; // Ví dụ: Tuyến 01: Bến xe Gia Lâm - Bến xe Yên Nghĩa
+
+        public double TotalDistanceKm { get; set; } // Tổng chiều dài tuyến (km)
+
+        public TimeSpan EstimatedDuration { get; set; } // Thời gian chạy dự kiến (vd: 01:30:00)
+
+        public bool IsActive { get; set; } = true;
+
+        // Quan hệ 1 - N: Một tuyến bao gồm danh sách nhiều trạm dừng theo thứ tự
+        public ICollection<RouteStop> RouteStops { get; set; } = new List<RouteStop>();
+    }
+}
